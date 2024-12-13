cc1cee835bdd0293c5e69695d2275c6e6ea18ad5
0omanuo0 <manu365manu@gmail.com>
2024-12-13 17:57:36 +0000

    change to uvw

---
+++ src/server.hpp
@@ -0,0 +1,176 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <uvw.hpp>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <iostream>
+#include <functional>
+#include <memory>
+#include <cstring>
+#include <map>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+
+
+#include "server_workers.hpp"
+#include "server_ssl.hpp"
+#include "logging.hpp"
+#include "httpMethods.hpp"
+#include "session.hpp"
+#include "response.hpp"
+#include "request.hpp"
+#include "jinjaTemplating/templating.h"
+
+class Templating;
+
+namespace server_types{
+    const std::map<std::string, std::string> content_type = {
+    {"js", "application/javascript"},
+    {"css", "text/css"},
+    {"html", "text/html"},
+    {"txt", "text/plain"}};
+
+    typedef variant<std::string, Response> HttpResponse;
+    typedef function<HttpResponse(Request&)> FunctionHandler;
+    typedef function<Response(Request&)> defaultFunctionHandler;
+
+    // typedef std::function<std::string()> FunctionHandler;
+
+
+    struct Route
+    {
+        std::string path;
+        std::vector<std::string> methods;
+        // types::HttpResponse cache_response;
+        bool cache_route;
+        FunctionHandler handler;
+    };
+    struct RouteFile
+    {
+        std::string path;
+        std::string type;
+    };
+
+    class HttpServerDefaults{
+    private:
+        std::string __default_not_found = "<h1>NOT FOUND</h1>";
+        defaultFunctionHandler __not_found_handler = [this](Request &req) -> Response {
+            return Response(__default_not_found, 404);
+        };
+        
+        std::string __default_unauthorized = "<h1>UNAUTHORIZED</h1>";
+        defaultFunctionHandler __unauthorized_handler = [this](Request &req) -> Response {
+            return Response(__default_unauthorized, 401);
+        };
+
+        std::string __default_internal_server_error = "<html><head><title>500 Internal Server Error</title></head><body><h1>500 Internal Server Error</h1><p>The server encountered an internal error or misconfiguration and was unable to complete your request.</p></body></html>";
+        defaultFunctionHandler __internal_server_error_handler = [this](Request &req) -> Response {
+            return Response(__default_internal_server_error, 500);
+        };
+
+        std::string __default_bad_request = "<html><head><title>400 Bad Request</title></head><body><h1>400 Bad Request</h1><p>Your browser sent a request that this server could not understand.</p></body></html>";
+        defaultFunctionHandler __redirect_handler = [this](Request &req) -> Response {
+            return Response(__default_bad_request, 400);
+        };
+    public:
+        Response getNotFound(Request &req) { return __not_found_handler(req); }
+        Response getUnauthorized(Request &req) { return __unauthorized_handler(req); }
+        Response getInternalServerError(Request &req) { return __internal_server_error_handler(req); }
+        Response getBadRequest(Request &req) { return __redirect_handler(req); }
+
+
+        void setNotFound(const defaultFunctionHandler &not_found) { __not_found_handler = not_found; }
+        void setUnauthorized(const defaultFunctionHandler &unauthorized) { __unauthorized_handler = unauthorized; }
+        void setInternalServerError(const defaultFunctionHandler &internal_server_error) { __internal_server_error_handler = internal_server_error; }
+        void setBadRequest(const defaultFunctionHandler &bad_request) { __redirect_handler = bad_request; }
+
+    
+    };
+
+}
+
+
+class HttpServer
+{
+private:
+    // server event loop variables
+    std::shared_ptr<uvw::TCPHandle> server_;
+    std::shared_ptr<uvw::Loop> loop_ = uvw::Loop::getDefault();
+    WorkerPool::TaskQueue taskQueue_;
+    WorkerPool::WorkerPool workerPool_;
+
+    // server main functions
+    void _setup_server();
+    void _run_server();
+    int _handle_request(std::string request, std::shared_ptr<SSLClient> sslClient);
+
+    // server variables
+    int port_;
+    std::string ip_;
+
+    // SSL variables
+    SSL_CTX *ctx_ = nullptr;
+    std::string ssl_context_[2];
+
+    // server modules
+    idGenerator idGeneratorJWT = idGenerator("");
+    std::shared_ptr<Templating> template_render;
+    
+    // server routes, files and sessions
+    std::vector<server_types::Route> routes;
+    std::vector<server_types::RouteFile> routesFile;
+    std::vector<Session> sessions;
+
+    // defaults
+    std::string default_session_name = "SessionID";
+
+    // server aditional functions
+    int _find_match_session(std::string id);
+    Session _get_session(int index);
+    Session _set_new_session(Session session);
+
+public:
+    // server modules pub
+    server_types::HttpServerDefaults defaults;
+    logging logger_;
+
+    HttpServer(const std::string &ip, int port, const std::string ssl_context[]);
+    HttpServer() : HttpServer("127.0.0.1", 5000, std::array<std::string, 2>{std::string(""), std::string("")}.data()) {}
+
+    void addRoute(const std::string &path, const std::vector<std::string> &methods, const server_types::FunctionHandler &handler)
+        { this->routes.push_back({path, methods, false, [handler](Request& req) -> server_types::HttpResponse { return handler(req); }}); }
+
+    void addRouteFile(string endpoint, const string &extension)
+    {
+        // if the route dont start with / add it
+        if(endpoint[0] != '/') endpoint = "/" + endpoint;
+
+        // if exists
+        for(auto &route : routesFile)
+            if(route.path == endpoint)
+                return;
+
+        auto it = server_types::content_type.find(extension);
+        std::string contentType = (it != server_types::content_type.end()) ? it->second : "application/force-download";
+
+        this->routesFile.push_back({endpoint, contentType});
+    }
+
+    void run();
+
+    std::string Render(const std::string &route, nlohmann::json data = nlohmann::json());
+
+    void urlfor(const std::string &path){
+        size_t index = path.find_last_of(".");
+        string extension = "txt";
+        if (string::npos != index)
+            extension = path.substr(index + 1);
+        addRouteFile(path, extension);
+    }
+};
+
+#endif
---
+++ src/tools/complementary_server_functions.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+#include <regex>
+
+
+namespace server_tools
+{
+
+    bool _route_contains_params(const std::string &routePath)
+    {
+        const std::regex paramRegex("<[^>]+>");
+        return std::regex_search(routePath, paramRegex);
+    }
+
+    bool _match_path_with_route(const std::string &path, const std::string &routePath, std::unordered_map<std::string, std::string> &url_params)
+    {
+        const std::regex routeRegex("^" + std::regex_replace(routePath, std::regex("<([^>]+)>"), "([^/]+)") + "$");
+        std::smatch matches;
+
+        if (!std::regex_match(path, matches, routeRegex))
+        {
+            return false;
+        }
+
+        std::regex varNameRegex("<([^>]+)>");
+        auto varNameBegin = std::sregex_iterator(routePath.begin(), routePath.end(), varNameRegex);
+        auto varNameEnd = std::sregex_iterator();
+
+        for (std::sregex_iterator i = varNameBegin; i != varNameEnd; ++i)
+        {
+            std::smatch match = *i;
+            std::string varName = match.str(1);
+            url_params[varName] = matches[std::distance(varNameBegin, i) + 1].str();
+        }
+
+        return true;
+    }
+
+}
