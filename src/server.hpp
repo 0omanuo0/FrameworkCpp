5c34e67a8f890420b331025bd7deb27e19c20313
0omanuo0 <manu365manu@gmail.com>
2025-06-05 20:42:05 +0000

    add webp mime

--- src/server.hpp
+++ src/server.hpp
@@ -42,6 +42,7 @@ namespace server_types
         {"jpg", "image/jpeg"},
         {"jpeg", "image/jpeg"},
         {"png", "image/png"},
+        {"webp", "image/webp"},
         {"gif", "image/gif"},
         {"svg", "image/svg+xml"},
         {"ico", "image/x-icon"},
